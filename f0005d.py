184c84cd0e78a99cfa9221840ea5431ac747f6fb
solymosiviktor1 <72075604+solymosiviktor1@users.noreply.github.com>
2020-11-03 14:56:21 +0100

    Add files via upload

---
+++ f0005d.py
@@ -0,0 +1,9 @@
+szl = 72
+sz2 = 3
+eredmény = szl + sz2
+tipp = input('mennyi ' + str(szl) + '+' + str(sz2) +  ' ? ')
+tipp = int(tipp)
+if tipp == eredmény:
+    print('Nem rossz, nem rossz...')
+else:
+    print('Ejnye!')
